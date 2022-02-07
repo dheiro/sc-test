8294f86f6e28c1aa806e3464d7b8d48fbd6ad182
Ade Yudha Pratama <ade@dheiro.com>
2022-02-07 12:57:23 +0700

    add user model

---
+++ src/models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    default: 'user',
+  }
+}, {
+  timestamps: true,
+});
+
+module.exports = mongoose.model('User', UserSchema);
